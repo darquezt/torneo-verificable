e698ad8d9bf49ca0ca075e4f454536059bfc169f
Damian Arquez <damian.arquez@ing.uchile.cl>
2019-08-24 15:20:37 -0400

    Add the life

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+
+import { makeStyles } from '@material-ui/core/styles'
 import NewTournament from './pages/NewTournament';
 import VerifyRandomness from './pages/VerifyRandomness';
 import SignOnTeam from './pages/SignOnTeam';
+import Header from './components/Header'
+import Background from './assets/img/parallax.jpeg'
+
+const useStyles = makeStyles(() => ({
+  content: {
+    backgroundImage: `url(${Background})`,
+    height: 'calc(100vh - 130px)',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+}))
 
 const App = () => {
+  const classes = useStyles()
+
   return (
     <>
-      <div>Hosdassdla!</div>
-      <Switch>
-        <Route path='/' exact component={NewTournament} />
-        <Route path='/verificar' exact component={VerifyRandomness} />
-        <Route path='/inscripcion' exact component={SignOnTeam} />
-      </Switch>
+      <Header />
+      <div className={classes.content}>
+        <Switch>
+          <Route path='/' exact component={NewTournament} />
+          <Route path='/verificar' exact component={VerifyRandomness} />
+          <Route path='/inscripcion' exact component={SignOnTeam} />
+        </Switch>
+      </div>
     </>
   );
 }
