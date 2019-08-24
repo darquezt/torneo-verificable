3acd6ac5322a77236ccae0aa9a2b43d50fe829f3
Mortup <g.uribemontero@gmail.com>
2019-08-24 14:39:23 -0300

    Add team sign up form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import NewTournament from './pages/NewTournament';
 import VerifyRandomness from './pages/VerifyRandomness';
+import SignOnTeam from './pages/SignOnTeam';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Switch>
         <Route path='/' exact component={NewTournament} />
         <Route path='/verificar' exact component={VerifyRandomness} />
+        <Route path='/inscripcion' exact component={SignOnTeam} />
       </Switch>
     </>
   );
